8c78bfa375e27c987fab5219f27039c29a795075
Daurenov <3350698@mail.ru>
2022-02-22 23:11:11 +0600

    Запись в ексель

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import json
 import csv
 import telebot
+import pandas as pd
 
 
 def getSchedule():
@@ -9,23 +10,46 @@ def getSchedule():
     try:
         response = requests.get(
             'https://kdp.aqnietgroup.com/v1/workplaces/205',
-            params={'date_from': '2021-07-01', 'date_to': '2021-07-01'},
+            params={'date_from': '2021-07-01', 'date_to': '2021-07-02'},
             headers={'Authorization': 'Bearer hOnIRtv-QpC84Ri0aZVRbukoxI3Z7iDr'},
         )
 
-        data = response.text
-
-        todos = json.loads(data)
-
+        data = response.json()
+        dict_sample = {}
         # dict_keys(['id', 'pharmacy', 'personal_number', 'working_day', 'date_from', 'date_to', 'day_off'])
-        for i in todos['items']:
-            values = i.values()
-            print(values)
-            # send_message(values, '555299761')
+
+        Name_list = []
+        working_day_list =[]
+        date_from_list = []
+        date_to_list = []
+        day_off = []
+
+        for i in data['items']:
+            #values = i.values()
+            values = list(i.values())
+
+            Name_list.append(values[2])
+            working_day_list.append(values[3])
+            date_from_list.append(values[4])
+            date_to_list.append(values[5])
+            day_off.append(values[6])
+
+            #print(values)
+
+
+        alaries1 = pd.DataFrame({'Name': Name_list,
+                                 'working_day': working_day_list,
+                                 'date_from': date_from_list,
+                                 'date_to': date_to_list,
+                                 'day_off': day_off,
+                                 })
+        print(alaries1)
+
+
+
+         # send_message(values, '555299761')
     except:
         print('Ошибка основного запроса')
-
-
 
 
 
@@ -67,7 +91,11 @@ def GetEmployeeName(table_number):
         print('Ошибка при получений данных по сотруднику')
 
 
-#getSchedule()
-GetEmployeeName('EUA-515065')
+alaries1 = pd.DataFrame({'Name': ['L. Messi', 'Cristiano Ronaldo', 'J. Oblak'],
+                                     'Salary': [560000, 220000, 125000]})
+#print(alaries1)
+
+getSchedule()
+#GetEmployeeName('EUA-515065')
 
 #send_message("Тест из другого модуля", "555299761")
